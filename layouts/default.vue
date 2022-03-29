3628231cce242593db2d71250d0bfcb8db7e6d58
Alex Largo <alexlar163@gmail.com>
2022-03-29 12:03:21 -0500

    Add component Header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app dark>
+    <es-header />
     <Nuxt />
   </v-app>
 </template>
 
 <script>
+import EsHeader from "@/components/EsHeader";
 export default {
   name: "DefaultLayout",
+  components: {
+    EsHeader,
+  },
   data() {
     return {};
   },
